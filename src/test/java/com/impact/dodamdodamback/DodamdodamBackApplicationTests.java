cd6794e13e9906344ca927615619a9212efdba80
JjungminLee <julie0964@naver.com>
2023-11-19 12:08:42 +0900

    init setting

---
+++ src/test/java/com/impact/dodamdodamback/DodamdodamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.dodamdodamback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
